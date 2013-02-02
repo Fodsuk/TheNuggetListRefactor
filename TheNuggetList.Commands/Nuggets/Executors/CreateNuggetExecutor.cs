35ca79d204809d9a48b22b2aac18fcaef230a6cb
Mike <fodsuk@hotmail.com>
2013-02-02 12:59:19 +0000

    - added Radiator and set up commanding

---
+++ TheNuggetList.Commands/Nuggets/Executors/CreateNuggetExecutor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Radiator.Core.Commanding;
+using Radiator.Core;
+
+namespace TheNuggetList.Commands.Nuggets.Executors
+{
+    public class CreateNuggetExecutor : CommandExecutor<CreateNuggetCommand>
+    {
+        public override ProcessResult ExecuteCommand(ICommandService commandService, CreateNuggetCommand command)
+        {
+            return new ProcessResult()
+            {
+                Successful = true
+            };
+        }
+    }
+}
---
+++ TheNuggetList.Commands/Nuggets/Validators/CreateNuggetValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Radiator.Core.Commanding;
+using Radiator.Core;
+
+namespace TheNuggetList.Commands.Nuggets.Validators
+{
+    public class CreateNuggetValidator : CommandValidator<CreateNuggetCommand>
+    {
+        public override ProcessResult ValidateCommand(CreateNuggetCommand command)
+        {
+            return new ProcessResult()
+            {
+                Successful = true                
+            };
+        }
+    }
+}
