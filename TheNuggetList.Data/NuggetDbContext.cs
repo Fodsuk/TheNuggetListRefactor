4cbba2803732a1d817900642d9f4e09a69452ad3
Chris Newark <chrisnewark@gmail.com>
2013-02-03 13:49:42 +0000

    Added members to domain

--- TheNuggetList.Data/NuggetDbContext.cs
+++ TheNuggetList.Data/NuggetDbContext.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Data.Entity;
 using TheNuggetList.Domain.Nuggets;
+using TheNuggetList.Domain.Members;
 
 namespace TheNuggetList.Data
 {
     public class NuggetDbContext : DbContext
-    {
+	{
+		public DbSet<Member> Members { get; set; }
         public DbSet<Nugget> Nuggets { get; set; }
     }
 }
---
+++ TheNuggetList.Domain/Members/Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheNuggetList.Domain.Members
+{
+	public class Member
+	{
+		public int Id { get; set; }
+		public int Username { get; set; }
+		public int EmailAddress { get; set; }
+		public int Password { get; set; }
+		public DateTime Created { get; set; }
+	}
+}
