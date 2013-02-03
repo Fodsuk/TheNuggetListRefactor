787be6a3f3fc30bd0630d78be02050b28e366398
Chris Newark <chrisnewark@gmail.com>
2013-02-03 16:47:27 +0000

    Added members controller

---
+++ TheNuggetList/Controllers/MembersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheNuggetList.Controllers
+{
+    public class MembersController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+		public ActionResult Logout()
+		{
+			return View();
+		}
+    }
+}
