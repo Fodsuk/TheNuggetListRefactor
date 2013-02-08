5fc43090c6543d95b9acb95ecc0bb83a6e6342ac
Mike <fodsuk@hotmail.com>
2013-02-08 23:01:20 +0000

    - Abstracted nugget list item

---
+++ TheNuggetList/Util/DateTimeFormater.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheNuggetList.Util
+{
+    public class DateTimeFormater
+    {
+        public string GetReadableTimespan(DateTime date)
+        {
+            return "I Can Typing";
+        } 
+    }
+}
--- TheNuggetList/ViewModels/HomeNuggetListItemViewModel.cs
+++ TheNuggetList/ViewModels/HomeNuggetListItemViewModel.cs
@@ -6,6 +6,7 @@ namespace TheNuggetList.ViewModels
     {
         public string Title { get; set; }
         public string Description { get; set; }
+        public string PostedBy { get; set; }
         public DateTime Created { get; set; }
     }
 }
