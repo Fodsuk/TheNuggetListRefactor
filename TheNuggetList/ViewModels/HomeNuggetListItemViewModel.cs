585ebc483adb6732cb4ea7170d0b2611b84bc0b9
Mike <fodsuk@hotmail.com>
2013-02-08 22:38:41 +0000

    - I Can Typing

---
+++ TheNuggetList/ViewModels/HomeNuggetListItemViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TheNuggetList.ViewModels
+{
+    public class HomeNuggetListItemViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
